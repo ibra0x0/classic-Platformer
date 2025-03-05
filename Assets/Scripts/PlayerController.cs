e45d8a03df872b02ded8e03b99dd764bf1fb9dda
ibra0x0 <kochbati.ibrahim@gmail.com>
2025-03-06 00:35:16 +0100

    Player movement x and y

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+    Rigidbody2D rb;
+    public bool IsMoving { get; private set; }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +22,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocityY);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
     }
 }
