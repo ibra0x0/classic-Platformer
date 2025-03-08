ffaff3d6f4ca7fbe841368e746c6ac4b1c538e0c
ibra0x0 <kochbati.ibrahim@gmail.com>
2025-03-08 01:14:03 +0100

    refactoring animation strings

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,7 +38,7 @@ public class PlayerController : MonoBehaviour
         private set {
 
             _isMoving = value;
-            animator.SetBool("isMoving", value);
+            animator.SetBool(AnimationStrings.isMoving, value);
 
 
         }
@@ -55,7 +55,7 @@ public class PlayerController : MonoBehaviour
         {
 
             _isRunning = value;
-            animator.SetBool("isRunning", value);
+            animator.SetBool(AnimationStrings.isRunning, value);
         }
     }
 
