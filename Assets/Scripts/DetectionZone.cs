35e0ed0a10a56da0406cc0be7416d5576db4a129
ibra0x0 <kochbati.ibrahim@gmail.com>
2025-03-12 00:55:03 +0100

    knight enemy attack

---
+++ Assets/Scripts/DetectionZone.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DetectionZone : MonoBehaviour
+{
+
+    public List<Collider2D> detectedColliders = new List<Collider2D>();
+    Collider2D col;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        detectedColliders.Add(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        detectedColliders.Remove(collision);
+
+    }
+
+
+   
+}
--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -6,16 +6,10 @@ public class Knight : MonoBehaviour
 {
 
     public float walkSpeed = 3f;
+    public DetectionZone attackZone;
     public enum WalkableDirection { right, left };
-    public WalkableDirection WalkDirection;
-    TouchingDirections touchingDirections;
-    Rigidbody2D rb;
-    private Vector2 walkDirectionVector = Vector2.right;
-    private bool hasFlipped = false;
-
-
-
-    private WalkableDirection _walkDirection
+    Animator animator;
+    public WalkableDirection WalkDirection
     {
         get { return _walkDirection; }
         set
@@ -32,16 +26,51 @@ public class Knight : MonoBehaviour
             }
             _walkDirection = value;
         }
-       
+
     }
 
-   
+    public bool CanMove
+    {
+        get
+        {
+            return animator.GetBool(AnimationStrings.canMove);
+        }
+    }
+
+    public bool _hasTarget = false;
+    public bool HasTarget
+    {
+        get { return _hasTarget; }
+        private set
+        {
+            _hasTarget = value;
+            animator.SetBool(AnimationStrings.hasTarget, value);
+        }
+    }
+
+    TouchingDirections touchingDirections;
+    Rigidbody2D rb;
+    public Vector2 walkDirectionVector = Vector2.right;
+    private bool hasFlipped = false;
+
+
+
+    private WalkableDirection _walkDirection;
+    private float walkRateSpeed = 0.06f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        HasTarget = attackZone.detectedColliders.Count > 0;
+    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         touchingDirections = GetComponent<TouchingDirections>();
-       
+        animator = GetComponent<Animator>();
+
     }
     public void FlipDirection()
     {
@@ -54,37 +83,25 @@ public class Knight : MonoBehaviour
             WalkDirection = WalkableDirection.right;
         }
         else
-        { 
+        {
             Debug.LogError("Current walkable direction is not set to legal values of right and left");
         }
     }
 
     private void FixedUpdate()
     {
-        if (!hasFlipped && touchingDirections.IsOnWall && touchingDirections.IsGrounded)
+        if (touchingDirections.IsOnWall && touchingDirections.IsGrounded)
         {
             FlipDirection();
-            hasFlipped = true;
+
         }
-        else if (!touchingDirections.IsOnWall || !touchingDirections.IsGrounded)
-        {
-            hasFlipped = false; // Reset the flag if conditions are not met
-        }
-        rb.linearVelocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.linearVelocityY);
+        if (CanMove)
+
+            rb.linearVelocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.linearVelocityY);
+
+        else
+            rb.linearVelocity = new Vector2(Mathf.Lerp(rb.linearVelocityX,0,walkRateSpeed), rb.linearVelocityY);
+        
     }
-
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-    }
+}
 
